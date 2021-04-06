0e9bc50aaee4ebc10f339a1476b370bf1644ed2f
Alexander Gvozdev <gvozdev-work@mail.ru>
2021-04-06 22:55:56 +0700

    top-slider

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ function cleanDist() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
